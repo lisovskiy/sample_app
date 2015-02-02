34503d1397c770af29ab09a92c5f0620bacb4589
lisovskiy_or <lisovskiyvladimir@gmail.com>
2015-02-02 20:08:30 +0300

    red test add

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,12 +11,12 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("Sample App | Home")
     end
   end
   
   describe "Help page" do
-
+  
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
@@ -24,7 +24,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title("Sample App | Help")
     end
   end
 
@@ -37,6 +37,20 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
+      expect(page).to have_title("Sample App | About")
+    end
+  end
+  
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Samle App | Contact")
     end
   end
 end
